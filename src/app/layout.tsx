cf6b6e997a3899bff26b600d9b5d1848bf189536
CertainlyMohneesh <133652369+certainlyMohneeesh@users.noreply.github.com>
2024-12-13 03:12:16 +0530

    typo error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import ClientLayout from "@/components/common/ClientLayout";
 import "@/styles/globals.css";
-import "@/prosemirror.css";
+import "@/styles/prosemirror.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
